670822f0ca5e0972aa908a1ec171f44061f9d2a4
Quinn Rohlf <qr@qrohlf.com>
2014-02-26 09:57:41 -0800

    Add icomoon demo link

--- app/config.rb
+++ app/config.rb
@@ -3,6 +3,7 @@ configure do
     set :app_name, "Hackathon Starter Sinatra"
     set :nav_left, {                                            # Navigation: 
         'Example View' => '/examples',                          # you can specify the url as a string
+        'Icomoon Demo' => '/css/vendor/icomoon/font_demo.html',
         'Example Menu Dropdown' => {                            # or you can pass a hash of other items
             'dropdown item 1' => '#',                           # to create a dropdown
             'dropdown item 2' => '#',
