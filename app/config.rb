45d6fe02107c509f09bc60021cf1f4ba84601442
Quinn Rohlf <qr@qrohlf.com>
2014-03-09 18:35:05 -0700

    Fix navbar on md screens and make profile box responsive

--- app/config.rb
+++ app/config.rb
@@ -5,7 +5,7 @@ configure do
         'Model Example' => '/examples',                          # you can specify the url as a string
         'Payment Example' => '/payment',
         'Icons List' => '/css/vendor/icomoon/font_demo.html',
-        'Example Menu Dropdown' => {                            # or you can pass a hash of other items
+        'Dropdown' => {                            # or you can pass a hash of other items
             'dropdown item 1' => '#',                           # to create a dropdown
             'dropdown item 2' => '#',
             'dropdown item 3' => '#'
