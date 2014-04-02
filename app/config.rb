d63b9d4ae61125637b62c672970c29411798d13d
Quinn Rohlf <qr@qrohlf.com>
2014-04-01 17:14:45 -0700

    use glyphsearch as the icons reference

--- app/config.rb
+++ app/config.rb
@@ -10,8 +10,8 @@ configure do
         'Payment Example' => '/payment',
         'Reference' => {
             'View on GitHub' => 'https://github.com/qrohlf/hackathon-starter-sinatra',
-            'Icons List' => '/vendor/icomoon/index.html',
-            'Glyph List' => 'http://getbootstrap.com/components/#glyphicons',
+            'Icomoon Reference' => 'http://glyphsearch.com/?library=icomoon',
+            'Glyphicons Reference' => 'http://glyphsearch.com/?library=glyphicons',
             'Bootstrap' => 'http://getbootstrap.com/css/',
             'More Tools' => 'https://github.com/qrohlf/Tools#readme',
             'More Themes' => 'http://bootswatch.com'
